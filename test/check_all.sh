846b837583bb00e2679aae98b6dd11ad8cccc973
Nicolas DUBIEN <dubzzz@users.noreply.github.com>
2017-01-31 18:09:16 +0100

    Update check_all.sh

--- test/check_all.sh
+++ test/check_all.sh
@@ -1,43 +1,84 @@
 err=0
 
-time ./test/check_output.sh -n 5  # number_repr is the base case
+# Generate number
+
+time ./test/check_output.sh -n 5    # one figure number is the base case for all the remaining
 if (( $? != 0 )); then err=1; fi;
 
-time ./test/check_output.sh -c 5  # char_repr @ case # 1 -- from number_repr
+time ./test/check_output.sh -n 2017 # multiple figures numbers can be generated in various ways, it must remains a number
 if (( $? != 0 )); then err=1; fi;
 
-time ./test/check_output.sh -c f  # char_repr @ case # 2 -- from undefined
+time ./test/check_output.sh -n -5   # negative numbers require a conversion to String to append '-'
 if (( $? != 0 )); then err=1; fi;
 
-time ./test/check_output.sh -c a  # char_repr @ case # 3 -- from NaN
+# Generate character
+# Various one-character tests to check various milestone values
+
+time ./test/check_output.sh -c 5
 if (( $? != 0 )); then err=1; fi;
 
-time ./test/check_output.sh -c +  # char_repr @ case # 4 -- from 1e+308
+time ./test/check_output.sh -c f
 if (( $? != 0 )); then err=1; fi;
 
-time ./test/check_output.sh -c c  # char_repr @ case # 5 -- from function
+time ./test/check_output.sh -c a
 if (( $? != 0 )); then err=1; fi;
 
-time ./test/check_output.sh -c r  # char_repr @ case # 6 -- from true
+time ./test/check_output.sh -c +
 if (( $? != 0 )); then err=1; fi;
 
-time ./test/check_output.sh -c s  # char_repr @ case # 7 -- from false
+time ./test/check_output.sh -c c
 if (( $? != 0 )); then err=1; fi;
 
-time ./test/check_output.sh -c B  # char_repr @ case # 8 -- from constructor name
+time ./test/check_output.sh -c r
 if (( $? != 0 )); then err=1; fi;
 
-time ./test/check_output.sh -c z  # char_repr @ case # 9 -- from a-z
+time ./test/check_output.sh -c s
 if (( $? != 0 )); then err=1; fi;
 
-time ./test/check_output.sh -c C  # char_repr @ case #10 -- from atob("20N")
+time ./test/check_output.sh -c B
 if (( $? != 0 )); then err=1; fi;
 
-time ./test/check_output.sh -c Z  # char_repr @ case #11 -- from fromCharCode
+time ./test/check_output.sh -c z
+if (( $? != 0 )); then err=1; fi;
+
+time ./test/check_output.sh -c C
+if (( $? != 0 )); then err=1; fi;
+
+time ./test/check_output.sh -c Z
+if (( $? != 0 )); then err=1; fi;
+
+time ./test/check_output.sh -c {
+if (( $? != 0 )); then err=1; fi;
+
+time ./test/check_output.sh -c }
+if (( $? != 0 )); then err=1; fi;
+
+# Generate strings
+
+time ./test/check_output.sh -t "find"
+if (( $? != 0 )); then err=1; fi;
+
+time ./test/check_output.sh -t "constructor"
+if (( $? != 0 )); then err=1; fi;
+
+time ./test/check_output.sh -t "fromCharCode"
+if (( $? != 0 )); then err=1; fi;
+
+time ./test/check_output.sh -t "abcdefghijklmnopqrstuvwxyz"
+if (( $? != 0 )); then err=1; fi;
+
+time ./test/check_output.sh -t "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 if (( $? != 0 )); then err=1; fi;
 
 time ./test/check_output.sh -t "Hello"
 if (( $? != 0 )); then err=1; fi;
 
+# Generate scripts
+
+time ./test/check_output.sh -s "(function(){console.log('Hello');})()"
+if (( $? != 0 )); then err=1; fi;
+
+time ./test/check_output.sh -s "(function(name) { var today = new Date(); var cmas_yr = today.getMonth() == 11 && today.getDate() > 25 ? today.getFullYear()+1 : today.getFullYear(); var cmas = new Date(cmas_yr, 11, 25); console.log('Hello ' + name + ', today we are the ' + today.toLocaleDateString('en-US') + '. Time to wait before Christmas: ' + Math.ceil((cmas.getTime() - today.getTime())/(1000*60*60*24)) + ' days'); })('Test')"
+if (( $? != 0 )); then err=1; fi;
 
 exit $err
