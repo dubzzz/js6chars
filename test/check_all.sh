167d44d49b7cf7457bed094a7a5dbcad540f42c7
Nicolas DUBIEN <dubzzz@users.noreply.github.com>
2017-03-07 15:30:16 +0100

    Add tricky characters in the test

--- test/check_all.sh
+++ test/check_all.sh
@@ -73,7 +73,7 @@ if (( $? != 0 )); then err=1; fi;
 time ./test/check_output.sh -t "Hello"
 if (( $? != 0 )); then err=1; fi;
 
-time ./test/check_output.sh -t ",;:!?./§*-+^_|@()[]{}#~&$£€"
+time ./test/check_output.sh -t ",;:!?./§*-+^_|@()[]{}#~&$£€\"\\'"
 if (( $? != 0 )); then err=1; fi;
 
 # Generate scripts
